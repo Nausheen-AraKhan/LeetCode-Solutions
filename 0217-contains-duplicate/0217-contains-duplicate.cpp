2f5cd712d7bf659c1f689223f33c184140513fec
Nausheen Ara Khan <nausheenara1909khan@gmail.com>
2026-09-10 20:47:30 +0530

    Time: 23 ms (92.59%), Space: 82.9 MB (80.28%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int i=0,j=1;
+        while(i<j && j<n)
+        {
+            if(nums[i]==nums[j])
+               return true;
+            i++;
+            j++;
+        }
+        return false;
+    }
+};
